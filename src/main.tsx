96209b6eef8c31974fb14b940807f6f57e7845e4
Bk49 <ericngyongwei@gmail.com>
2023-09-29 12:20:20 +0800

    Complete notistack notification components

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
+import { SnackbarProvider } from "notistack";
+import ErrorMessage from "./components/common/message/ErrorMessage";
+import InfoMessage from "./components/common/message/InfoMessage";
+import SuccessMessage from "./components/common/message/SucessMessage";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <SnackbarProvider
+            Components={{
+                error: ErrorMessage,
+                success: SuccessMessage,
+                info: InfoMessage,
+            }}
+        >
+            <RouterProvider router={router} />
+        </SnackbarProvider>
     </React.StrictMode>
 );
