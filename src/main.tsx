0a3e168af4302a196efea4808262738398d8c942
Bk49 <ericngyongwei@gmail.com>
2023-09-29 12:23:34 +0800

    Complete tanstack query client setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,40 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
-import { SnackbarProvider } from "notistack";
+import { SnackbarProvider, enqueueSnackbar } from "notistack";
 import ErrorMessage from "./components/common/message/ErrorMessage";
 import InfoMessage from "./components/common/message/InfoMessage";
 import SuccessMessage from "./components/common/message/SucessMessage";
+import {
+    QueryCache,
+    QueryClient,
+    QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+    queryCache: new QueryCache({
+        onError: (error, query) => {
+            if (typeof query?.meta?.errorMessage === "string") {
+                enqueueSnackbar(query.meta.errorMessage, { variant: "error" });
+            } else if (error instanceof Error) {
+                enqueueSnackbar(error.message, { variant: "error" });
+            }
+        },
+    }),
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <SnackbarProvider
-            Components={{
-                error: ErrorMessage,
-                success: SuccessMessage,
-                info: InfoMessage,
-            }}
-        >
-            <RouterProvider router={router} />
-        </SnackbarProvider>
+        <QueryClientProvider client={queryClient}>
+            <SnackbarProvider
+                Components={{
+                    error: ErrorMessage,
+                    success: SuccessMessage,
+                    info: InfoMessage,
+                }}
+            >
+                <RouterProvider router={router} />
+            </SnackbarProvider>
+        </QueryClientProvider>
     </React.StrictMode>
 );
