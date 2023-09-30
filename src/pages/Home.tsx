f865a7fec28b9c0213684c6fabf767b3bb743f8c
Bk49 <ericngyongwei@gmail.com>
2023-09-30 14:35:19 +0800

    Add Banner to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import NavBar from "../components/common/navigations/NavBar";
+import Banner from "../components/common/banner/Banner";
 
 const Home: React.FC = () => {
     return <>
         <NavBar />
+        <Banner />
     </>;
 };
 
