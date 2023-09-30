892ede2aa6763c1bad56e3fa1ccf39271fb4d4c5
Bk49 <ericngyongwei@gmail.com>
2023-09-30 18:48:49 +0800

    Create necessary shipTypes constant

---
+++ src/constants/shiptypes.ts
@@ -0,0 +1,11 @@
+const shipTypes = [
+    "Tugboat",
+    "Container Ship",
+    "Bulk Carrier",
+    "Oil Tanker",
+    "LNG Carrier (Liquefied Natural Gas Carrier)",
+    "LPG Carrier (Liquefied Petroleum Gas Carrier)",
+    "Ro-Ro Ship (Roll-on/Roll-off Ship)",
+];
+
+export default shipTypes
