5ebfdf6571925155d0b2e9911a175f96b46b3094
Bk49 <ericngyongwei@gmail.com>
2023-09-30 13:38:16 +0800

    Import and use theme in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,8 @@ import {
     QueryClient,
     QueryClientProvider,
 } from "@tanstack/react-query";
+import { ThemeProvider } from "@mui/material";
+import defaultTheme from "./assets/theme";
 
 const queryClient = new QueryClient({
     queryCache: new QueryCache({
@@ -27,15 +29,17 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
-            <SnackbarProvider
-                Components={{
-                    error: ErrorMessage,
-                    success: SuccessMessage,
-                    info: InfoMessage,
-                }}
-            >
-                <RouterProvider router={router} />
-            </SnackbarProvider>
+            <ThemeProvider theme={defaultTheme}>
+                <SnackbarProvider
+                    Components={{
+                        error: ErrorMessage,
+                        success: SuccessMessage,
+                        info: InfoMessage,
+                    }}
+                >
+                    <RouterProvider router={router} />
+                </SnackbarProvider>
+            </ThemeProvider>
         </QueryClientProvider>
     </React.StrictMode>
 );
