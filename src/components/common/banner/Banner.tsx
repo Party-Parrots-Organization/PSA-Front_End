9405288391795b6d5978a1ae604fa86ca99e2075
Bk49 <ericngyongwei@gmail.com>
2023-09-30 14:34:52 +0800

    Create app banner

---
+++ src/components/common/banner/Banner.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PortImg from "../../../assets/image/Port.png";
+import { Grid, Typography } from "@mui/material";
+
+const Banner: React.FC = () => {
+    return (
+        <Grid
+            container
+            direction="row"
+            sx={{
+                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${PortImg})`,
+                height: { xs: "45vh", sm: "50vh", md: "55vh", lg: "60vh" },
+                width: "100vw",
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+                backgroundRepeat: "no-repeat",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            <Typography variant="h1" color="white">
+                Ship Arrival Estimator
+            </Typography>
+        </Grid>
+    );
+};
+
+export default Banner;
