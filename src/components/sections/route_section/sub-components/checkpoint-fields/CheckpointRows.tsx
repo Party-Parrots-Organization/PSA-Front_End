924c8e8759bf3ee7934bf9d804ce75d200df0329
Bk49 <ericngyongwei@gmail.com>
2023-09-30 18:49:55 +0800

    Create CheckpointRows for dynamic field rows

---
+++ src/components/sections/route_section/sub-components/checkpoint-fields/CheckpointRows.tsx
@@ -0,0 +1,64 @@
+import IconButton from "@mui/material/IconButton";
+import RemoveIcon from "@mui/icons-material/RemoveCircleOutline";
+import DragHandle from "@mui/icons-material/DragHandle";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import React from "react";
+import PortSelectField from "../../../../common/form/PortSelectField";
+import { Grid } from "@mui/material";
+
+interface CheckpointRowsType {
+    id: number;
+    removeFn: () => void;
+}
+
+const CheckpointRows: React.FC<CheckpointRowsType> = ({ id, removeFn }) => {
+    const {
+        attributes,
+        listeners,
+        setNodeRef,
+        transform,
+        transition,
+        isDragging,
+    } = useSortable({ id });
+
+    return (
+        <Grid
+            {...attributes}
+            component="div"
+            ref={setNodeRef}
+            container
+            direction="row"
+            alignItems="center"
+            sx={{
+                columnGap: { xs: "0.5rem", sm: "1rem" },
+                transform: CSS.Transform.toString(transform),
+                transition,
+            }}
+        >
+            <Grid item>
+                <IconButton onClick={removeFn}>
+                    <RemoveIcon />
+                </IconButton>
+            </Grid>
+
+            <Grid item lg={4} md={5} sm={6} xs={8}>
+                <PortSelectField
+                    label={`Checkpoint Port ${id + 1}`}
+                    name={`checkpoints.${id}`}
+                />
+            </Grid>
+
+            <Grid
+                item
+                component="div"
+                style={{ cursor: isDragging ? "grabbing" : "grab" }}
+                {...listeners}
+            >
+                <DragHandle />
+            </Grid>
+        </Grid>
+    );
+};
+
+export default CheckpointRows;
