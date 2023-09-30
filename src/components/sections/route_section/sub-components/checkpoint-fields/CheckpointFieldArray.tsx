bb86223aef1dc6f72c967f9e5c1afc2c7ac09330
Bk49 <ericngyongwei@gmail.com>
2023-09-30 18:50:21 +0800

    Create CheckPointArray for dynamic field rows

---
+++ src/components/sections/route_section/sub-components/checkpoint-fields/CheckpointFieldArray.tsx
@@ -0,0 +1,85 @@
+import { useFieldArray } from "react-hook-form";
+import CheckpointRows from "./CheckpointRows";
+import {
+    DndContext,
+    closestCenter,
+    KeyboardSensor,
+    PointerSensor,
+    useSensor,
+    useSensors,
+} from "@dnd-kit/core";
+import {
+    SortableContext,
+    sortableKeyboardCoordinates,
+    verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { Grid } from "@mui/material";
+import Button from "../../../../common/button/Button";
+import AddIcon from "@mui/icons-material/Add";
+
+const CheckPointFieldArray = () => {
+    const { fields, append, remove, move } = useFieldArray({
+        name: "checkpoints",
+    });
+    const sensors = useSensors(
+        useSensor(PointerSensor),
+        useSensor(KeyboardSensor, {
+            coordinateGetter: sortableKeyboardCoordinates,
+        })
+    );
+
+    return (
+        <>
+            <DndContext
+                onDragEnd={(event) => {
+                    const { active, over } = event;
+                    if (!!over && active.id !== over.id) {
+                        const oldIndex = fields.findIndex(
+                            (field) => field.id === active.id
+                        );
+                        const newIndex = fields.findIndex(
+                            (field) => field.id === over.id
+                        );
+
+                        move(oldIndex, newIndex);
+                    }
+                }}
+                sensors={sensors}
+                collisionDetection={closestCenter}
+            >
+                <SortableContext
+                    strategy={verticalListSortingStrategy}
+                    items={fields.map((field) => field.id)}
+                >
+                    <Grid direction="column" rowGap="1rem">
+                        {fields.map((field, index) => (
+                            <CheckpointRows
+                                key={field.id}
+                                id={index}
+                                removeFn={() => {
+                                    remove(
+                                        fields.findIndex(
+                                            ({ id }) => id === field.id
+                                        )
+                                    );
+                                }}
+                            />
+                        ))}
+                    </Grid>
+                </SortableContext>
+            </DndContext>
+            <Grid item>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    endIcon={<AddIcon />}
+                    onClick={() => append("")}
+                >
+                    Add Checkpoints
+                </Button>
+            </Grid>
+        </>
+    );
+};
+
+export default CheckPointFieldArray;
