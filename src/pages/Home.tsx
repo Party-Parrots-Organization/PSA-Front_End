f57a95bd201c9f0b68c72b3c1162b587b3822b22
Bk49 <ericngyongwei@gmail.com>
2023-09-30 18:56:00 +0800

    Add all completed components to Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import NavBar from "../components/common/navigations/NavBar";
 import Banner from "../components/common/banner/Banner";
+import { Grid } from "@mui/material";
+import RouteInformationSection from "../components/sections/route_section/RouteInformationSection";
+import { FormProvider, useForm } from "react-hook-form";
 
 const Home: React.FC = () => {
-    return <>
-        <NavBar />
-        <Banner />
-    </>;
+    const formState = useForm();
+
+    return (
+        <FormProvider {...formState}>
+            <NavBar />
+            <Banner />
+            <Grid container direction="column" columnGap="3rem">
+                <Grid item>
+                    <RouteInformationSection />
+                </Grid>
+                <Grid item></Grid>
+            </Grid>
+        </FormProvider>
+    );
 };
 
 export default Home;
