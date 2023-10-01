2c78454bd61a7cc8bba51fb8f9b27ff428b46a67
Bk49 <ericngyongwei@gmail.com>
2023-10-01 16:30:50 +0800

    Fix false configuration of instance.ts

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-const configureInstance = (instanceUrl: string, withCredentials?: boolean) => ({
+const configureInstance = (instanceUrl: string) => ({
     baseURL: `${
         import.meta.env.DEV
             ? import.meta.env.VITE_DEVELOPMENT_SERVER
             : import.meta.env.VITE_PRODUCTION_SERVER
     }/api/${import.meta.env.VITE_API_VERSION}/${instanceUrl}`,
     setTimeout: 1000,
-    withCredentials: withCredentials,
 });
 
-export const generalInstance = axios.create(configureInstance("/", false));
+export const instance = axios.create(
+    configureInstance(``)
+);
